f9e698a452570c19e0201c9f1d6bfacc2637f66a
Jiawei <kurobafear@hotmail.com>
2017-01-03 09:57:14 +0800

    change some thing

--- App/Admin/Controller/UserController.class.php
+++ App/Admin/Controller/UserController.class.php
@@ -13,11 +13,44 @@ class UserController extends AdminController {
 		}
 		$role = $this->page('UserView',30,null,$where);
 		$this->assign('user',$role);
+
+		$Role = M('Role');
+		$roles = $Role->select();
+		$this->assign('role',$roles);
+
 		$this->display();
 	}
 
 	public function del($id){
 		$this->del_adapt('User',$id,false);
+	}
+
+	public function add(){
+		if(!IS_POST){
+			$this->error('请求方式错误');
+		}
+		$Model = D('Node');
+		$data = $Model->create();
+		$data['create_time'] = date('Y-m-d H:i:s');
+		if($Model->add($data)){
+			$this->success('成功添加');
+		}else{
+			$this->error('失败添加');
+		}
+	}
+
+	public function upd(){
+		if(!IS_POST){
+			$this->error('请求方式错误');
+		}
+		$Model = D('Node');
+		$data = $Model->create();
+		$data['create_time'] = date('Y-m-d H:i:s');
+		if($Model->save($data)){
+			$this->success('成功修改');
+		}else{
+			$this->error('失败修改');
+		}
 	}
 
 }
