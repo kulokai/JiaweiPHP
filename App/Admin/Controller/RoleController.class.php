c60aeb00dc3fd1f09097b1ecb080af9d9aab87fc
Jiawei <kurobafear@hotmail.com>
2017-01-03 10:11:08 +0800

    change some thing

--- App/Admin/Controller/RoleController.class.php
+++ App/Admin/Controller/RoleController.class.php
@@ -19,4 +19,32 @@ class RoleController extends AdminController {
 		$this->del_adapt('Role',$id);
 	}
 
+	public function add(){
+		if(!IS_POST){
+			$this->error('请求方式错误');
+		}
+		$Model = D('Role');
+		$data = $Model->create();
+		$data['create_time'] = date('Y-m-d H:i:s');
+		if($Model->add($data)){
+			$this->success('成功添加');
+		}else{
+			$this->error('失败添加');
+		}
+	}
+
+	public function upd(){
+		if(!IS_POST){
+			$this->error('请求方式错误');
+		}
+		$Model = D('Role');
+		$data = $Model->create();
+		$data['create_time'] = date('Y-m-d H:i:s');
+		if($Model->save($data)){
+			$this->success('成功修改');
+		}else{
+			$this->error('失败修改');
+		}
+	}
+
 }
