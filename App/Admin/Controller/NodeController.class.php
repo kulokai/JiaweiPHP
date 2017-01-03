534b99b86c1b6abcb337c0dde9380d1f2c8bf4af
Jiawei <kurobafear@hotmail.com>
2017-01-03 09:36:14 +0800

    change some thing

--- App/Admin/Controller/NodeController.class.php
+++ App/Admin/Controller/NodeController.class.php
@@ -25,4 +25,32 @@ class NodeController extends AdminController {
 		$this->del_adapt('Node',$id);
 	}
 
+	public function add(){
+		if(!IS_POST){
+			$this->error('请求方式错误');
+		}
+		$Model = D('Node');
+		$data = $Model->create();
+		$data['create_time'] = date('Y-m-d H:i:s');
+		if($Model->add($data)){
+			$this->success('成功添加');
+		}else{
+			$this->error('失败添加');
+		}
+	}
+
+	public function upd(){
+		if(!IS_POST){
+			$this->error('请求方式错误');
+		}
+		$Model = D('Node');
+		$data = $Model->create();
+		$data['create_time'] = date('Y-m-d H:i:s');
+		if($Model->save($data)){
+			$this->success('成功修改');
+		}else{
+			$this->error('失败修改');
+		}
+	}
+
 }
